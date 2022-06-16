e650b36599767fdb45d71826fce13922f9bd5ed1
aramaramyan <aramaramyan1997@mail.ru>
2022-06-16 11:30:44 +0400

    feat: add firebase to project

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCpuugi23W9u1dYw6L5uSXzLAM0BRjmfNw",
+  authDomain: "ultra-todo-be19c.firebaseapp.com",
+  projectId: "ultra-todo-be19c",
+  storageBucket: "ultra-todo-be19c.appspot.com",
+  messagingSenderId: "904132867515",
+  appId: "1:904132867515:web:768bfe334703b51be3809e",
+});
+
+export default firebaseApp;
