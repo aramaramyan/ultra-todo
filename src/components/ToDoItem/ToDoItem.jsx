c7ffdac55d26dfee769840424402e4cd9f275f46
aramaramyan <aramaramyan1997@mail.ru>
2022-06-16 17:16:54 +0400

    fix: remove ESLint desable-next-line

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,7 +1,9 @@
 import watchIcon from "../../icons/watch.svg";
 import "./ToDoItem.scss";
 
-export default function ToDoItem() {
+const todoTitle = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.";
+
+  export default function ToDoItem() {
   return (
     <div className="todo">
       <div className="todo__content">
@@ -9,10 +11,7 @@ export default function ToDoItem() {
           <img src={watchIcon} alt="Watch Icon" />
           <p className="todo__status_title title">Pending</p>
         </div>
-        <p className="title">
-          {/* eslint-disable-next-line max-len */}
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolorem eos facilis minus possimus quasi.
-        </p>
+        <p className="title">{todoTitle}</p>
       </div>
       <div className="todo__button">
         <p className="todo__button_title title">Mark as done</p>
