62499d2f445e20af5aeb0e9cd50a542f79620eb6
aramaramyan <aramaramyan1997@mail.ru>
2022-06-16 11:59:34 +0400

    feat: add enviroment variables for firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyCpuugi23W9u1dYw6L5uSXzLAM0BRjmfNw",
-  authDomain: "ultra-todo-be19c.firebaseapp.com",
-  projectId: "ultra-todo-be19c",
-  storageBucket: "ultra-todo-be19c.appspot.com",
-  messagingSenderId: "904132867515",
-  appId: "1:904132867515:web:768bfe334703b51be3809e",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export default firebaseApp;
