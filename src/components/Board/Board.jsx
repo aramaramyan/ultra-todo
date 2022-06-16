323effb138e0fda9079725e88501b88a47d49f36
aramaramyan <aramaramyan1997@mail.ru>
2022-06-16 15:09:15 +0400

    feat: create UserItem.jsx and add styles

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,21 +1,26 @@
 import "./Board.scss";
 import { useState } from "react";
 import Modal from "../Modal/Modal";
+import UserItem from "../UserItem/UserItem";
 
-// const users = [
-//   { id: 1, fullName: "Peter Bishop", rate: 7 },
-//   { id: 2, fullName: "Olivia Dunham", rate: 62 },
-//   { id: 3, fullName: "Walter Bishop", rate: 14 },
-//   { id: 4, fullName: "William Bell", rate: 21 },
-//   { id: 5, fullName: "David Robert Jones", rate: 27 },
-//   { id: 6, fullName: "Phillip Broyles", rate: 100 },
-//   { id: 7, fullName: "Nina Sharp", rate: 94 },
-//   { id: 8, fullName: "Charley Franklin", rate: 35 },
-//   { id: 9, fullName: "Astrid Farnsworth", rate: 27 },
-// ];
+const users = [
+  { id: 1, fullName: "Peter Bishop", rate: 7 },
+  { id: 2, fullName: "Olivia Dunham", rate: 62 },
+  { id: 3, fullName: "Walter Bishop", rate: 14 },
+  { id: 4, fullName: "William Bell", rate: 21 },
+  { id: 5, fullName: "David Robert Jones", rate: 27 },
+  { id: 6, fullName: "Phillip Broyles", rate: 100 },
+  { id: 7, fullName: "Nina Sharp", rate: 94 },
+  { id: 8, fullName: "Charley Franklin", rate: 35 },
+  { id: 9, fullName: "Astrid Farnsworth", rate: 27 },
+];
 
 export default function Board() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
 
   function closeModal() {
     setIsModalOpen(false);
@@ -30,7 +35,16 @@ export default function Board() {
             <p className="board__title_rate">Completion rate(%)</p>
           </div>
         </div>
-        <div className="board__list" />
+        <div className="board__list">
+          {users.map(({ id, fullName, rate }, i) => <UserItem
+            key={id}
+            fullName={fullName}
+            rate={rate}
+            isBlue={i % 2}
+            /* eslint-disable-next-line react/jsx-no-bind */
+            openModal={openModal}
+          />)}
+        </div>
       </div>
       {/* eslint-disable-next-line react/jsx-no-bind */}
       {isModalOpen && <Modal closeModal={closeModal} />}
