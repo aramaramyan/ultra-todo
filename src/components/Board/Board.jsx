6077168c422632c9eb4480c5e3c55c58a6cfdf48
aramaramyan <aramaramyan1997@mail.ru>
2022-06-16 17:01:48 +0400

    fix: crate test data file

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,19 +1,8 @@
-import "./Board.scss";
 import { useState } from "react";
 import Modal from "../Modal/Modal";
 import UserItem from "../UserItem/UserItem";
-
-const users = [
-  { id: 1, fullName: "Peter Bishop", rate: 7 },
-  { id: 2, fullName: "Olivia Dunham", rate: 62 },
-  { id: 3, fullName: "Walter Bishop", rate: 14 },
-  { id: 4, fullName: "William Bell", rate: 21 },
-  { id: 5, fullName: "David Robert Jones", rate: 27 },
-  { id: 6, fullName: "Phillip Broyles", rate: 100 },
-  { id: 7, fullName: "Nina Sharp", rate: 94 },
-  { id: 8, fullName: "Charley Franklin", rate: 35 },
-  { id: 9, fullName: "Astrid Farnsworth", rate: 27 },
-];
+import users from "../../staticTestData";
+import "./Board.scss";
 
 export default function Board() {
   const [isModalOpen, setIsModalOpen] = useState(false);
