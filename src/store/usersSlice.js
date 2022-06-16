13afcaf305d9c6bde2151b4a80d6b998c80fa4a7
aramaramyan <aramaramyan1997@mail.ru>
2022-06-16 19:00:18 +0400

    feat: create redux store and users slice

---
+++ src/store/usersSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: [],
+  reducers: {
+    setUsers(state, action) {
+      state = action.payload;
+    }
+  }
+});
+
+export const { setUsers } = usersSlice.actions;
+export default usersSlice.reducer;
